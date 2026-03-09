cc20681a7aab0c2a6c3120d0ebe934b2e6573316
kumail-kmr25 <ka6307464@gmail.com>
2026-03-09 17:28:16 +0530

    Cleanup: Remove unnecessary temporary files, logs and utility scripts

--- tmp_test_prisma.js
+++
@@ -1,7 +0,0 @@
-const { PrismaClient } = require("@prisma/client");
-const prisma = new PrismaClient();
-async function test() {
-    console.log("Prisma Client loaded successfully");
-    await prisma.$disconnect();
-}
-test();
